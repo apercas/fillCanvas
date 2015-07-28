378af30a7ec8989ba190140123a356472eb40e83
Adrián Pérez <adrian@simbiotica.es>
2015-07-28 12:13:40 +0200

    bitwise operators and comparison

--- logic.js
+++ logic.js
@@ -40,6 +40,9 @@ document.querySelector('button').addEventListener('click', function () {
 			ctx3.fillRect([i],[j],1,1);
 			var pixel1 = ctxT.getImageData([i],[j],1,1).data;
 			var pixel2 = ctxT2.getImageData([i],[j],1,1).data;
+
+			// var color_match = (pixel1[0] == pixel2[0] && pixel1[1] == pixel2[1] && pixel1[2] == pixel2[2] && pixel1[3] == pixel2[3])
+
 			var color_match = Array.prototype.slice.apply(pixel1).every(function(element, index) {
 			    return element === ctxT2.getImageData([i],[j],1,1).data[index]; 
 			});
@@ -51,5 +54,39 @@ document.querySelector('button').addEventListener('click', function () {
 			}
 		}
 	}
+
+
+	// var imageData   	= ctxT.getImageData(0,0,300,300);
+	// var buf 			= new ArrayBuffer(imageData.data.length);
+	// var buf8 			= new Uint8ClampedArray(buf);
+	// var data 			= new Uint32Array(buf);
+
+	// var imageData2   	= ctxT2.getImageData(0,0,300,300);
+	// var buf_2 			= new ArrayBuffer(imageData.data.length);
+	// var buf8_2 			= new Uint8ClampedArray(buf);
+	// var data_2 			= new Uint32Array(buf);
+
+	// var imageData3   	= ctx3.getImageData(0,0,300,300);
+	// var buf_3 			= new ArrayBuffer(imageData.data.length);
+	// var buf8_3 			= new Uint8ClampedArray(buf);
+	// var data_3 			= new Uint32Array(buf);
+
+	// for (var y = 0; y < 300; ++y) {
+	//     for (var x = 0; x < 300; ++x) {
+	//         var iter = y * 300 + x;
+	//         if (imageData.data[iter] == imageData2.data[iter]) {
+	//         	data_3[iter] =
+	//             (255   << 24) |    // alpha
+	//             (0   << 16) |      // blue
+	//             (0   <<  8) |      // green
+	//              0;                // red
+	//         }
+	//     }
+	// }
+
+	// imageData.data.set(buf8);
+	// ctx3.putImageData(imageData, 0, 0);
+
+	
 	document.body.appendChild(cipo3);
 });
